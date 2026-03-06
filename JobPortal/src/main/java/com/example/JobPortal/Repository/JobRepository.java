55952a11c67ed53c7d26d73b2205686f410a0d6d
Prasanna G <prasannag1505@gmail.com>
2026-03-06 21:15:56 +0530

    Delete JobPortal directory

--- JobPortal/src/main/java/com/example/JobPortal/Repository/JobRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.JobPortal.Repository;
-
-import com.example.JobPortal.Entity.JobsEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobRepository extends JpaRepository<JobsEntity,Long> {
-}
--- JobPortal/src/main/java/com/example/JobPortal/Utils/AppUtils.java
+++
@@ -1,8 +0,0 @@
-package com.example.JobPortal.Utils;
-
-public class AppUtils {
-    public enum UserRoles{
-        JOBSEEKER,
-        EMPLOYER
-    }
-}
